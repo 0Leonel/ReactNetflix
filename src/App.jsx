c7859d17e907b56bba7222c855d1f6b31cfdd3f7
Leo <leonelandias@gmail.com>
2023-11-30 18:46:36 -0300

    merge UAT

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,6 @@ import RootProvider from "./core/providers/rootProvider";
 
 const App = () => {
   return (
-<<<<<<< HEAD
-    <div>
-      <h1>APP PARA PELIS</h1>
-      <h2>Esto Funciona para el cliente</h2>
-    </div>
-  );
-};
-=======
->>>>>>> origin/QA
-
       <>
       <RootProvider>
         <RouterProvider router={appRouter}/>
