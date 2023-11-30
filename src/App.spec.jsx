0bcc1cec7fd85e9985e285289b726e9161a6cd2d
Leo <leonelandias@gmail.com>
2023-11-30 18:34:22 -0300

    fix

--- src/App.spec.jsx
+++ src/App.spec.jsx
@@ -5,7 +5,7 @@ import { describe, expect, it } from "vitest";
 
 describe("App", () => {
   it("App", () => {
-    const component = render(<App />);
+    const component = render();
 
     expect(component).toBeDefined();
   });
